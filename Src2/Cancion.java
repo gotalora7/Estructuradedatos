aa6791fee34a21ea544ca914c962c523c04a2226
Gedots <gotalora7@gmail.com>
2026-01-23 02:26:52 +0000

    Se esta creando la play list

---
+++ Src2/Cancion.java
@@ -0,0 +1,23 @@
+public class Cancion {
+    private String titulo;
+    private String artista;
+    private int duracion;
+
+    public Cancion(String titulo, String artista, int duracion) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+}
---
+++ Src2/principal.java
@@ -0,0 +1,6 @@
+Class principal {
+    public static void main(String[] args) {
+        Cancion cancion = new Cancion("Hasta la madre", "Jeison Jimenez", 355);
+
+    }
+}
