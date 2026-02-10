c3ccee6ecffdc6b9cf6525b7d821461e6e96d5d9
Gedots <gotalora7@gmail.com>
2026-02-10 02:12:19 +0000

    Agragar codigo

--- Clima act 2/Estacionclimatica.java
+++ Clima act 2/Estacionclimatica.java
@@ -1,7 +1,25 @@
-package Clima act 2;
-
 public class Estacionclimatica {
     
-    double[] temperaturas = new double[24];
+     private double[] temperaturas = new double[24];
 
+    public Estacionclimatica() {
+        for (int i = 0;i temperaturas.length;i++){
+            temperaturas[i] = Math.random()*40;
+
+        }
+    } 
+    public void mostrartemperaturamediodia() {
+        System.out.println("temperatura medio dia" + temperaturas[12]+ "°c");
+    }
+    public void mostrartemperaturainvalidada() {
+        try {
+            temperaturas[24] = Math.random()*40;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("error no se puede vertemperatura en 24 horas");
+            System.out.println("java solo tiene memoria fija");
+
+    }
+
+
+    
 }
