7996fd90c023a8eae0f64b10a5be4f6ae26a4edd
Gedots <gotalora7@gmail.com>
2026-02-24 01:29:59 +0000

    Act 8.2

--- Sistema de tickets Act8/MainSoporte.java
+++ Sistema de tickets Act8/MainSoporte.java
@@ -4,24 +4,21 @@ public class MainSoporte {
 
         System.out.println("Apertura del Sistema de Tickets");
         
-        // Paso 2: Llegan clientes
         soporte.registrarCliente("Cliente A (Problema de Internet)");
         soporte.registrarCliente("Cliente B (Falla de Software)");
         soporte.registrarCliente("Cliente C (Duda de Facturación)");
 
-        // Paso 3: Consultamos quién es el primero
         System.out.println();
         soporte.verSiguiente();
 
-        // Paso 4: Atendemos a los clientes en orden FIFO
         System.out.println();
-        soporte.atenderCliente(); // Atiende al A
-        soporte.atenderCliente(); // Atiende al B
+        soporte.atenderCliente();
+        soporte.atenderCliente();
         
-        System.out.println("\n--- Estado después de atender ---");
-        soporte.verSiguiente(); // Ahora el siguiente es el C
+        System.out.println("Estado después de atender");
+        soporte.verSiguiente();
         
-        soporte.atenderCliente(); // Atiende al C
-        soporte.atenderCliente(); // Intenta atender con fila vacía (usa .poll)
+        soporte.atenderCliente();
+        soporte.atenderCliente();
     }
 }
